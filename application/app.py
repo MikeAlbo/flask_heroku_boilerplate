3c0b03b2c3cb36ab3dd5b17bff0c9f98b7cd9741
Michael Albonetti <malbo24@gmail.com>
2019-08-08 11:41:12 -0600

    clean up app.py

--- application/app.py
+++ application/app.py
@@ -12,10 +12,9 @@ def create_app(**config_override):
     app = Flask(__name__)
     app.config.from_object(os.environ['APP_SETTINGS'])
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    # print("app settings", os.environ['APP_SETTINGS'])
     app.config.update(config_override)
 
-    # ini the db
+    # init the db
     db.init_app(app)
     migrate.init_app(app, db)
 
@@ -28,10 +27,6 @@ def create_app(**config_override):
     @app.route("/", methods=['GET'])
     def re_dir():
         return redirect(url_for('api.get_users'))
-
-    # @app.route("/")
-    # def test_route():
-    #     return "test route working \n"
 
     return app
 
